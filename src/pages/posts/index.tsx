d0a381ffc1b09b01e233e57bfeb0bf6095c7c6ef
seungyeon.lee <dus3214@gmail.com>
2021-12-27 00:26:20 +0900

    ✨ 글 목록 API 구성

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,25 @@
 import { NextPage } from "next";
+import  useSWR  from "swr";
 import { MDViewr } from "../../components/MDViewer";
+import { Post } from "../../types/Post";
+import { fetcher } from "../../utils/fetcher";
 
 const PostPage: NextPage = () => {
+  const { data, error } = useSWR('http://localhost:1337/api/posts/', fetcher);
+
+  if (error) return (<div>Error</div>)
+  if (!data) return (<div>Loading..</div>)
+
   return (
     <div>
-      <MDViewr title={'안녕하세요?'} contents='# hello'/>
+      {data.data.map((postdata: Post) => {
+        <MDViewr 
+        key={`post-${postdata.id}`}
+        author={postdata.attributes.author}
+        title={postdata.attributes.title} 
+        contents={postdata.attributes.contents}/>
+
+      })}
     </div>
   )
 }
