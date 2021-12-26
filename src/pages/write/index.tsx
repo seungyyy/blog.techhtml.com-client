f9b9f7d129537cc13e0e3905cd39bb0c36a0b446
seungyeon.lee <dus3214@gmail.com>
2021-12-27 00:32:59 +0900

    ✨ POST 추가

--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -1,5 +1,20 @@
+import axios from "axios";
 import { NextPage } from "next";
-import { useState } from "react";
+import React, { useState } from "react";
+
+const write = (title: string, post: string) => {
+  axios.post('http://localhost:1337/api/posts/',{
+    "data":  {
+      "author": "승연",
+      "title": title,
+      "contents": post,
+    }
+  }).then((res) => {
+    if(res.status === 200) {
+      location.href = '/posts'
+    }
+  })
+}
 
 
 const WritePage:NextPage = () => {
@@ -14,10 +29,15 @@ const WritePage:NextPage = () => {
     setTitle(e.target.value);
   }
 
+  const handleSubmit = (e:React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    write(title, post);
+  }
+
   return (
     <div>
       <h1>글 작성하기</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleTitleChange} value={title} />
         <textarea onChange={handleTextAreaChange} value={post} />
         <button type="submit">발행하기</button>
